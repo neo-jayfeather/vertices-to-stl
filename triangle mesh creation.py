491c4bc3c74f0737a4e59f3a4b2aa3ca957f550e
neo-jayfeather <156867325+neo-jayfeather@users.noreply.github.com>
2024-05-09 22:29:28 -0400

    stash update

--- triangle mesh creation.py
+++ triangle mesh creation.py
@@ -1,21 +1,13 @@
 import numpy as np # needed libraries
 import trimesh
-<<<<<<< Updated upstream
-path = 'canon_in_abL.txt'
-=======
 path = 'sRGB_in_abL.txt'
->>>>>>> Stashed changes
 pathName = path.split('\\')[len(path.split('\\'))-1] # complex way to get file name after last \
 pathName = path.split('.')[len(path.split('.'))-2] # complex way to remove .txt or ending
 num_vertices = 0 
 num_coordinates = 0
 triangles = []
 
-<<<<<<< Updated upstream
-with open('triangle_points1.txt', 'r') as file: # open text file
-=======
 with open(path, 'r') as file: # open text file
->>>>>>> Stashed changes
     lines = file.readlines()
     num_vertices = len(lines)
     num_coordinates = len(lines[0].rstrip().replace('\t',' ').split(' ')) #i hope this works, will test at home or something
@@ -23,19 +15,12 @@ with open(path, 'r') as file: # open text file
     for x in range(num_vertices):
         for y in range(num_coordinates):
             vertices[x][y] = float(lines[x].rstrip().replace('\t',' ').split(' ')[y]) # no pain
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
 for i in range(num_vertices):
     triangles.append([3 * i, 3 * i + 1, 3 * i + 2]) # triangle array defenition
     # triangle one uses verticies 0,1,2 etc.
 
 vertices_array = np.array(vertices).reshape(-1, 3) # helpful
 mesh = trimesh.Trimesh(vertices=vertices_array, faces=triangles) #mesh creation
-<<<<<<< Updated upstream
-mesh.export(f'{pathName} output.stl', file_type='stl') # mesh created
-=======
 with open('sRGB_in_abL_color.txt' ,'r') as file:
     colors = file.readlines()
     num_lines = len(colors)
@@ -51,5 +36,4 @@ with open('sRGB_in_abL_color.txt' ,'r') as file:
         mesh.visual.face_colors[x] = allColors[x]
     
 mesh.export(f'{pathName} output.glb', file_type='glb') # mesh created
->>>>>>> Stashed changes
 print("done") #confirmation
