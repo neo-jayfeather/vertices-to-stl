f803ebfb73c49bf54eebad0946492b14e361f594
neo-jayfeather <156867325+neo-jayfeather@users.noreply.github.com>
2024-05-12 11:54:09 -0400

    smarter way to switch vertices
    it works now.

--- triangle mesh creation.py
+++ triangle mesh creation.py
@@ -1,34 +1,51 @@
 import numpy as np # needed libraries
 import trimesh
 import os
+
 path = 'sRGB_in_abL.txt'
-pathName = path.split('\\')[len(path.split('\\'))-1] # complex way to get file name after last \
-pathName = path.split('.')[len(path.split('.'))-2] # complex way to remove .txt or ending
-num_vertices = 0 
-num_coordinates = 0
+pathName = os.path.splitext(path)[0] #gets filename
+num_vertices, num_coordinates = 0,0
+custom = False # flag, enable to have custom vertices and coordinates
 triangles = []
+
+def get_avail_filename(base):
+    filename, ext = os.path.splitext(base)
+    count = 1
+    while os.path.exists(base):
+        base = f"{filename}({count}){ext}"
+        count += 1
+    return base
 
 with open(path, 'r') as file: # open text file
     lines = file.readlines()
-    num_vertices = len(lines)
-    num_coordinates = len(lines[0].rstrip().replace('\t',' ').split(' ')) #i hope this works, will test at home or something
+    if(not custom):
+        num_vertices = len(lines)
+        num_coordinates = len(lines[0].rstrip().replace('\t',' ').split(' ')) #i hope this works, will test at home or something
     vertices = [[0.0] * num_coordinates for _ in range(num_vertices)] # define list
-    for x in range(num_vertices):
+    #lines = np.array(lines).reshape(1,-1)
+    '''for x in range(num_vertices):
         for y in range(num_coordinates):
             vertices[x][y] = float(lines[x].rstrip().replace('\t',' ').split(' ')[y]) # no pain
         temp = vertices[x]
-        
         #vertices[x] = [temp[1],temp[0],temp[2],temp[4],temp[3],temp[5],temp[7],temp[6],temp[8]]  # Swap (0, 1)
         #vertices[x] = [temp[2],temp[1],temp[0],temp[5],temp[4],temp[3],temp[8],temp[7],temp[6]]
         vertices[x] = [temp[0], temp[2], temp[1], temp[3], temp[5], temp[4], temp[6], temp[8], temp[7]]
         #dumb solution to dumb problem, but it works
-        
+    '''
+    
+    for x in range(num_vertices):
+        for y in range(num_coordinates):
+            vertices[x][y] = float(lines[x].rstrip().replace('\t',' ').split(' ')[y]) # no pain
+    vertices = np.array(vertices).reshape(-1,3)
+    for x in range(len(vertices)):
+        a, b, c = 0, 2, 1
+        vertices[x] = [vertices[x][a],vertices[x][b],vertices[x][c]]
 for i in range(num_vertices):
     triangles.append([3 * i + 0, 3 * i + 1, 3 * i + 2]) # triangle array defenition
     # triangle one uses vertiies 0,1,2 etc.
 
-vertices_array = np.array(vertices).reshape(-1, 3) # helpful
-mesh = trimesh.Trimesh(vertices=vertices_array, faces=triangles) #mesh creation
+#vertices_array = np.array(vertices).reshape(-1, 3) # helpful
+mesh = trimesh.Trimesh(vertices=vertices, faces=triangles) #mesh creation
 with open('sRGB_in_abL_color.txt' ,'r') as file:
     colors = file.readlines()
     num_lines = len(colors)
@@ -42,14 +59,7 @@ with open('sRGB_in_abL_color.txt' ,'r') as file:
         allColors[x][3] = 255
         #print(allColors[x])
         mesh.visual.face_colors[x] = allColors[x]
-def get_avail_filename(base):
-    filename, ext = os.path.splitext(base)
-    new_filename = base
-    count = 1
-    while os.path.exists(new_filename):
-        new_filename = f"{filename}({count}){ext}"
-        count += 1
-    return new_filename
+
 output_filename = f"{pathName}_output.glb"
 available_filename = get_avail_filename(output_filename)
 mesh.export(available_filename, file_type='glb') # mesh created
